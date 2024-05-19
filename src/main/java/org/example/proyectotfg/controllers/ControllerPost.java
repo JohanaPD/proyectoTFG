b6fd6afe51f2a1d44cf47a4bd7d2c7994279d80d
JohanaPD <118165435+JohanaPD@users.noreply.github.com>
2024-05-19 21:10:50 +0200

    vistas post

---
+++ src/main/java/org/example/proyectotfg/controllers/ControllerPost.java
@@ -0,0 +1,23 @@
+package org.example.proyectotfg.controllers;
+
+import org.example.proyectotfg.mediators.Mediator;
+import org.example.proyectotfg.mediators.MediatorPost;
+import org.example.proyectotfg.mediators.ViewController;
+
+public class ControllerPost implements ViewController {
+    private MediatorPost mediator;
+    @Override
+    public void setMediator(Mediator mediador) {
+        this.mediator=(MediatorPost)mediador;
+    }
+
+    @Override
+    public MainController getMainController() {
+        return null;
+    }
+
+    @Override
+    public void setMainController(MainController mainController) {
+
+    }
+}
---
+++ src/main/java/org/example/proyectotfg/mediators/MediatorPost.java
@@ -0,0 +1,7 @@
+package org.example.proyectotfg.mediators;
+
+public interface MediatorPost {
+
+    void makePost();
+    void viewPost();
+}
